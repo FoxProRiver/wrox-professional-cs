4b4e7485ef38cfb5b54ef8d5bdef4caa6d4be729
FoxProRiver <273785259@qq.com>
2016-12-01 11:42:22 +0800

    提交第一个测试文件

---
+++ ch1/hello.cs
@@ -0,0 +1,10 @@
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        System.Console.WriteLine("Hello World!");
+        System.Console.WriteLine("Wrox Professional C# 4.0");
+    }
+}
